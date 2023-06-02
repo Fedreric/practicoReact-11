a5371abe46304bf74d94e1a5c02708f8e55a8c52
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-02 12:45:54 -0300

    Se hace la consulta a la API

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Container } from "react-bootstrap";
 import Formulario from "./components/Formulario";
-import ListaNoticias from "./components/ListaNoticias";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [noticias, setNoticias] = useState({});
+  useEffect(() => {
+    consultarApi();
+  }, []);
+  const consultarApi = async () =>{
+    try{
+      const respuesta = await fetch("https://newsdata.io/api/1/news?apikey=pub_2390338fd0d756307ef040ecd26608655dde4&q=pizza");
+
+      const dato = await respuesta.json();
+      setNoticias(dato)
+      console.log(dato)
+    }catch(e){
+      console.log(e)
+    }
+  }
   return (
     <>
       <h1 className="text-center display-3">NOTICIAS</h1>
       <hr />
       <Container>
         <Formulario></Formulario>
-        <ListaNoticias></ListaNoticias>
       </Container>
     </>
   );
