9e83374383dcce5e60ce1792c6f50c7a244e2747
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-02 11:11:01 -0300

    Se completa el maquetado del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,16 +2,21 @@ import { Form } from "react-bootstrap";
 
 const Formulario = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Buscar por categorias</Form.Label>
+    <Form className="container">
+      <Form.Group className="row align-items-center" controlId="formBasicEmail">
+        <div className="col-md-2">
+          <Form.Label>Buscar por categorias:</Form.Label>
+        </div>
+        <div className="col-md-10">
           <Form.Select id="categoria">
             <option>Categoria 1</option>
             <option>Categoria 2</option>
             <option>Categoria 3</option>
             <option>Categoria 4</option>
           </Form.Select>
+        </div>
       </Form.Group>
+      <hr></hr>
     </Form>
   );
 };
