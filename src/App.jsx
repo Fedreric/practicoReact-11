a9b1de33046deb5f6fb2c99fad95fafaec0336b4
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-02 10:40:25 -0300

    Se agrega el mquetado basico en app que contiene un titulo, hr y container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
-      <h1>titulo prueba bootstrap</h1>
+            <h1 className='text-center display-3'>NOTICIAS</h1>
+      <hr />
+      <Container>
+        
+      </Container>
     </>
   )
 }
