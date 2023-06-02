b435bb7198d4342dc216b736fb7549923232f3bd
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-02 12:12:35 -0300

    Se agrega el maquetado para mostrar las noticias

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,12 +1,23 @@
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Button } from "react-bootstrap";
 
 const Noticia = () => {
-    return (
-        <Col>
-           <Card>
-            </Card> 
-        </Col>
-    );
+  return (
+    <Col md={6} lg={3} className="my-2">
+      <Card>
+        <Card.Img variant="top" src="https://media.datacenterdynamics.com/media/images/Extra.width-880.jpg" />
+        <Card.Body>
+          <Card.Title>Titulo de noticia</Card.Title>
+          <Card.Subtitle className="text-muted">Autor</Card.Subtitle>
+          <Card.Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, a corporis suscipit magni itaque nemo doloremque modi aspernatur sapiente repellendus?
+          </Card.Text>
+          <Card.Footer>
+            <Button variant="primary">Ver noticia completa</Button>
+          </Card.Footer>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
 };
 
 export default Noticia;
