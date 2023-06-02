488c3f19f601644f843c7379fba3066ffa958946
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-02 11:14:49 -0300

    Se agrega el componente Noticia.jsx

---
+++ src/components/Noticia.jsx
@@ -0,0 +1,12 @@
+import { Card, Col } from "react-bootstrap";
+
+const Noticia = () => {
+    return (
+        <Col>
+           <Card>
+            </Card> 
+        </Col>
+    );
+};
+
+export default Noticia;
